6a17e469da9ce7585b090449ac70cc269458c76d
ibrahim muibi <ibrahimthesofttech@gmail.com>
2024-10-03 23:29:28 +0100

    pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        # Previous row is the last row in the triangle
+        prev_row = triangle[-1]
+        # Initialize the new row with a leading 1
+        new_row = [1]
+
+        # Populate the middle values
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+
+        # End the row with a trailing 1
+        new_row.append(1)
+
+        # Append the new row to the triangle
+        triangle.append(new_row)
+
+    return triangle
